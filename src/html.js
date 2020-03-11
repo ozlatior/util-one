0fb4dc3d450753b8183d8887028e575ed2f0a08d
Octav Zlatior <oektaw@gmail.com>
2020-03-11 11:06:44 +0800

    Implements html tag extraction functions
    * tag name and "role" extraction from tag
    * content extraction based on tags from a string
    
    TODO: only manual testing was done for this, implement proper tests

--- src/html.js
+++ src/html.js
@@ -5,7 +5,17 @@ const html = {
 	 */
 	regexp: {
 		COMMENT_START: "<!--",
-		COMMENT_END: "-->"
+		COMMENT_END: "-->",
+		TAG_START: "^ *< *[^/]",
+		TAG_END: "^ *< */ *[^/]",
+		TAG_CLOSED: "^ *< *[^<>/]+ */ *> *$"
+	},
+
+	/*
+	 * Tag groupings of various kinds
+	 */
+	tags: {
+		CONTENT: [ "a", "b", "del", "em", "i", "strong", "br" ]
 	},
 
 	/*
@@ -99,6 +109,98 @@ const html = {
 			let num = parseInt(numStr, 10);
 			return String.fromCharCode(num);
 		});
+	},
+
+	/*
+	 * Extract the name of a tag from tag
+	 */
+	getTagName: function(str) {
+		str = str.replace(/[<>\/]/g, "").trim();
+		let tag = str.split(" ")[0].toLowerCase();
+		return tag;
+	},
+
+	/*
+	 * Determine if it's opening, closing or single tag (no content, like <br/>
+	 */
+	getTagRole: function(str) {
+		if (str.match(html.regexp.TAG_CLOSED) !== null)
+			return "closed";
+		if (str.match(html.regexp.TAG_END) !== null)
+			return "end";
+		if (str.match(html.regexp.TAG_START) !== null)
+			return "start";
+	},
+
+	/*
+	 * Break up content by tags - returns a list of lines and blocks of formatting, eg
+	 * str: "<b>Name:</b> John Doe <i>(intern)</i></br><b>Address:</b> Foo Bar" will yield an array of two lines
+	 * [
+	 *     { tags: [ "b" ], content: "Name:" }, { tags: [], content: " John Doe " }, { tags: [ "i" ], content: "(intern)" },
+	 *     { tags: [ "b" ], content: "Address:" }, { tags: [], content: " Foo Bar" }
+	 * ]
+	 */
+	breakByTag: function(str) {
+		let ret = [];
+		let reg = new RegExp("< *(/ *)?(" + html.tags.CONTENT.join("|") + ")( +[^<>]+)? *>", "g");
+		// first, break into lines
+		str = str.replace(/<\/? *br *\/?>/g, "<br>").split("<br>");
+		for (let i=0; i<str.length; i++) {
+			let m = str[i].match(reg);
+			// no tags found, we have a single element with no tags
+			if (m === null) {
+				ret.push([ { tags: [], content: str[i] } ]);
+				continue;
+			}
+			// get information about the tags
+			let k = -1;
+			let tags = [];
+			for (let j=0; j<m.length; j++) {
+				k = str[i].indexOf(m[j], k+1);
+				tags.push({
+					str: m[j],
+					tag: html.getTagName(m[j]),
+					role: html.getTagRole(m[j]),
+					index: k,
+					length: m[j].length
+				});
+			}
+			k = 0;
+			// make sure there are no open ends (they will mess up the algorithm further down the path)
+			// however, unclosed tags are ok since we'll close everything at the end of the string
+			while (tags[k].role === "end") {
+				tags.unshift({ str: "", tag: tags[k].tag, role: "start", index: 0, length: 0});
+				k += 2;
+			}
+			// now build a line with fragments of the text and the tags
+			let line = [];
+			let openTags = [];
+			let lastIndex = 0;
+			for (let j=0; j<tags.length; j++) {
+				// check that we have some new content between the consecutive tags
+				if (tags[j].index !== lastIndex) {
+					line.push({
+						tags: openTags.slice(0),
+						content: str[i].slice(lastIndex, tags[j].index)
+					});
+				}
+				// add it or remove it from open tags
+				if (tags[j].role === "start")
+					openTags.push(tags[j].tag);
+				if (tags[j].role === "end")
+					openTags.splice(tags.indexOf(tags[j].tag, 1));
+				// calculate new "lastIndex"
+				lastIndex = tags[j].index + tags[j].length;
+			}
+			// push what's left, if anything is left
+			if (lastIndex < str[i].length-1)
+				line.push({
+					tags: openTags.slice(0),
+					content: str[i].slice(lastIndex)
+				});
+			ret.push(line);
+		};
+		return ret;
 	}
 
 };
